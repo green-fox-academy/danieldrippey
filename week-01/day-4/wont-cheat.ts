2624960eee5f2cd58b1baa1285ca21c32b9dd731
Daniel Drippey <danieldrippey@gmail.com>
2019-06-27 17:22:02 +0200

    TS basic practise with loops

---
+++ week-01/day-4/wont-cheat.ts
@@ -0,0 +1,15 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let a: number = 0;
+while (a < 10) {
+  console.log("I won't cheat on the exam!"); 
+  a++;
+}
+
+for (let i: number = 0; i < 10; i++) {
+    console.log("I won't cheat on the exam!"); 
+  }
+  
