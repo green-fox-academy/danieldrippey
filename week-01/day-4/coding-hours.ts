d095a1fe0e5905b511b55a1bbfc790cbd31a4579
Daniel Drippey <danieldrippey@gmail.com>
2019-06-27 12:57:53 +0200

    TS practice with percentages

---
+++ week-01/day-4/coding-hours.ts
@@ -0,0 +1,14 @@
+// An average Green Fox attendee codes 6 hours daily
+// The semester is 17 weeks long
+//
+// Print how many hours is spent with coding in a semester by an attendee,
+// if the attendee only codes on workdays.
+//
+// Print the percentage of the coding hours in the semester if the average
+// work hours weekly is 52
+
+
+'use strict';
+
+console.log("An average GF attendee codes " + 6 * 17 * 5 + " hours in a semester." );
+console.log((((30 * 17) / (52 * 17)) * 100) + "that is the average percentage the Gf attendees'bcoding hours.");
