2940859b64c56167ca1dc958e536f96790a95a1f
Daniel Drippey <danieldrippey@gmail.com>
2019-06-27 14:21:25 +0200

    TS basic practice with variable types

---
+++ week-01/day-4/define-basics.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+
+let myName: string = "Dani";
+let myAge: number = 34;
+let myHeight: number = 1.89;
+let amIMarried: boolean = false;
+
+console.log(myName);
+console.log(myAge);
+console.log(myHeight);
+console.log(amIMarried);
