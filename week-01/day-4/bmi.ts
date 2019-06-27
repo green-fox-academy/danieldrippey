701e1124a29e8e76fd81d3754861f9b772175fa5
Daniel Drippey <danieldrippey@gmail.com>
2019-06-27 14:12:42 +0200

    TS arithmetic operations

---
+++ week-01/day-4/bmi.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+console.log("Your BMI is " + massInKg /  (heightInM ** 2));
+
+
+
+
+// Print the Body mass index (BMI) based on these values
