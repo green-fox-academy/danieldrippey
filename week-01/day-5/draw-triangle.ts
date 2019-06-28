0e0c195c789a1e978f14d7b8642a429f312299b9
Daniel Drippey <danieldrippey@gmail.com>
2019-06-28 12:46:32 +0200

    TS basic practice with for loop and string variable

---
+++ week-01/day-5/draw-triangle.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+let lineCount: number = 4;
+let lineCount2: string = '';
+for(let i: number = 1; i < lineCount +1 ; i++){
+   lineCount2 = lineCount2 + '*';
+   console.log(lineCount2);
+}
+
+
+
+
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+
+
