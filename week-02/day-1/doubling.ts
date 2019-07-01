313746298b657981ae405bad4ecd19568e2f5ffa
Daniel Drippey <danieldrippey@gmail.com>
2019-07-01 21:28:26 +0200

    workshop functions - double an integer

---
+++ week-02/day-1/doubling.ts
@@ -0,0 +1,14 @@
+ 'use strict'
+ 
+ let baseNum: number = 123;
+
+ function doubling (number) { 
+    let doubledValue: number = number * 2;
+    console.log(doubledValue);
+ }
+ doubling (baseNum);
+
+
+// -  Create a variable named `baseNum` and assign the value `123` to it
+// -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// -  Log the result of `doubling(baseNum)` to the console
