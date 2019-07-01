65c01dc18834260839f3c11a61c33f671b7a14d1
Daniel Drippey <danieldrippey@gmail.com>
2019-07-01 21:47:06 +0200

    function workshop - greetings

---
+++ week-02/day-1/greet.ts
@@ -0,0 +1,13 @@
+'use strict'
+
+let nameToGreet: string = "Green Fox";
+
+function greet (name){
+    console.log("Greetings, dear " + name);
+}
+greet(nameToGreet);
+
+// -  Create variable named `nameToGreet` and assign the value `Green Fox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Green Fox`
+// -  Greet `nameToGreet`
