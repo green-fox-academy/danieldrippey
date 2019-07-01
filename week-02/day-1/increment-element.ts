fb31ea1e646f14d8bd299650d831e6e98161a04c
Daniel Drippey <danieldrippey@gmail.com>
2019-07-01 14:45:25 +0200

    array workshop - changing an element's value in an array

---
+++ week-02/day-1/increment-element.ts
@@ -0,0 +1,11 @@
+'use strict'
+
+let numList: number[] = [1, 2, 3, 4, 5];
+//let incremented = numList.splice(2, 1, 10);
+//console.log(incremented[2]);
+numList[2] += 1;
+console.log(numList[2]);
+
+// -  Create a variable named `numList` with the following content: `[1, 2, 3, 4, 5]`
+// -  Increment the third element simply by accessing it
+// -  Log the third element to the consoles
