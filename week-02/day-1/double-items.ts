5383326db0fdc87dc434a4ee1010860d514399b4
Daniel Drippey <danieldrippey@gmail.com>
2019-07-01 15:28:08 +0200

    array workshop - map function - duplicate items

---
+++ week-02/day-1/double-items.ts
@@ -0,0 +1,20 @@
+'use strict'
+
+// -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+let drinks: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'].map(function(i){
+    return i + i;
+})
+console.log(drinks);
+
+
+
+
+//let drinks: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+//drinks.forEach(function(item){
+//    item + item;
+//    console.log(drinks);
+//})
+
