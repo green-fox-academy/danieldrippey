9b28da42bdde92534bf4d00065d7bf3aecb34b71
Daniel Drippey <danieldrippey@gmail.com>
2019-07-01 12:43:30 +0200

    array workshop task

---
+++ week-02/day-1/third.ts
@@ -0,0 +1,8 @@
+'use strict'
+
+//Create a variable named `magicNumbers`
+//    with the following content: `[1, 3, 5, 7]`
+// -  Print the third element of `magicNumbers
+
+let MagicNumbers: number[] = [1, 3, 5, 7];
+console.log(MagicNumbers[2]);
