8eaea248921bc448cebdb761daa30589f0607fad
Daniel Drippey <danieldrippey@gmail.com>
2019-08-01 10:47:47 +0200

    matrix practice for the exam

---
+++ week-05/rotate-matrix.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+
+let MatrixToRotate : number[][] = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+function matrixRotater (inputMatrix: number[][]): number[][]  {
+    let rotatedMatrix: number[][] = [];
+    for (let i: number = 0; i < inputMatrix.length; i++) {
+        let newRow: number[] = [];
+        for (let j: number = inputMatrix.length- 1; j >= 0; j--) {
+            newRow.push(inputMatrix[j][i]);
+        }
+    rotatedMatrix.push(newRow);
+    }
+    return rotatedMatrix;
+} 
+
+console.log(matrixRotater(MatrixToRotate));
